11e9ed209e559fde41a73771cc93a8ace3dab408
Ben Dalton <ben.dalton@technekes.com>
2014-07-28 07:51:14 -0400

    fixed another thing

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,15 +1,11 @@
-// create angular app
 	var validationApp = angular.module('validationApp', []);
 
-	// create angular controller
 	validationApp.controller('validationController', function($scope) {
 
-		// function to submit the form after all validation has occurred			
 		$scope.submitForm = function() {
 
-			// check to make sure the form is completely valid
 			if ($scope.accountSettings.$valid) {
-				alert('our form is amazing');
+
 			}
 
 		};
